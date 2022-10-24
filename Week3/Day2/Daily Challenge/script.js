939966ff54634056ae48cbff36c9e79afb21e005
Hanna8KH <hannarozen98@gmail.com>
2022-10-25 01:54:53 +0300

    week 3 day 2 exercises

---
+++ Week3/Day2/Daily Challenge/script.js
@@ -0,0 +1,212 @@
+//Exercise 1
+
+// let myFavFood = 'eggs with avocado';
+// let myFavMeal = 'breakfast';
+// let sentence = 'I eat' + 'myFavFood'
+// console.log("I eat " + myFavFood +  " at every " + myFavMeal);
+
+//Exercise 2 - (I gave up with this one and last one (5/6) exercises... I'll have to repeat it all again on weelends after work)
+
+// const myWatchedSeries = ["black mirror", "money heist", "the big bang theory"];
+// const myWatchedSeriesLength = myWatchedSeries.length
+// console.log('myWatchedSeriesLength:', myWatchedSeriesLength)
+// const myWatchedSeriesSentence = '${myWatchedSeries[0]}, ${myWatchedSeries[1]}, ${myWatchedSeries[2]}'
+// console.log('myWatchedSeriesSentence')
+// const sentence = 'I watched ${myWatchedSeriesLength} series: ${myWatchedSeriesSentence}'
+
+
+// myWatchedSeries[2]= 'friends'
+// console.log(myWatchedSeries)
+
+// myWatchedSeries.push('Hundred')
+// console.log(myWatchedSeries)
+
+// myWatchedSeries.unshift('SpiesFamily')
+// console.log(myWatchedSeries)
+
+// delete myWatchedSeries[1]
+// console.log(myWatchedSeries)
+
+// const moneyHeist = myWatchedSeries[2]
+// console.log(moneyHeist[2])
+
+// console.log(myWatchedSeries)
+
+
+
+
+
+// Exercise 3 : The Temperature Converter
+
+// const celsiusT = 20;
+// const farenheitT = celsiusT / 5 * 9 + 32
+// console.log(farenheitT)
+
+
+// Exercise 4 : Guess The Answers #1
+
+// For example
+
+// console.log(2+3)
+// // Prediction: It will output 5, because 2 and 3 are numbers
+// // Actual: 5
+
+
+// Using the code below:
+
+//     let c;
+//     let a = 34;
+//     let b = 21;
+
+//     console.log(a+b) //first expression
+//     // Prediction: 55 - cuz a and b are numbers
+//     // Actual: 55
+
+//     a = 2;
+
+//     console.log(a+b) //second expression
+//     // Prediction: 23 - cuz a and b still  numbers
+//     // Actual:
+
+
+
+// What will be the outcome of a + b in the first expression ? = 55 
+// What will be the outcome of a + b in the second expression ? = 23
+// What is the value of c? = undefined
+
+// Analyse the code below, what will be the outcome?
+// console.log(3 + 4 + '5'); = 75 - concatination 
+
+// Exercise 5 : Guess The Answers #2
+
+// What is the output of each of the expressions below?
+
+
+// typeof(15)
+// // Prediction: number
+// // Actual: number
+
+// typeof(5.5)
+// // Prediction: number
+// // Actual: number
+
+// typeof(NaN)
+// // Prediction: none
+// // Actual: number
+
+// typeof("hello")
+// // Prediction: string
+// // Actual: string
+
+// typeof(true)
+// // Prediction: boolean
+// // Actual: boolean
+
+// typeof(1 != 2)
+// // Prediction: -
+// // Actual: boolean
+
+// "hamburger" + "s"
+// // Prediction: hamburgers
+// // Actual: hamburgers
+
+// "hamburgers" - "s"
+// // Prediction: hamburger
+// // Actual: NaN
+
+// "1" + "3"
+// // Prediction: 13
+// // Actual: 13
+
+// "1" - "3"
+// // Prediction: 2
+// // Actual: -2 
+
+// "johnny" + 5
+// // Prediction: johnny5
+// // Actual: johnny5
+
+// "johnny" - 5
+// // Prediction:NaN
+// // Actual:NaN
+
+// 99 * "hello"
+// // Prediction:NaN
+// // Actual:NaN
+
+// 1 != 1
+// // Prediction:
+// // Actual:
+
+// 1 == "1"
+// // Prediction:false
+// // Actual:false
+
+// 1 === "1"
+// // Prediction:true
+// // Actual:true
+
+
+// Exercise 6 : Guess The Answers #3
+
+// What is the output of each of the expressions below?
+
+
+// 5 + "34"
+// // Prediction:39
+// // Actual:534
+
+// 5 - "4"
+// // Prediction:nan
+// // Actual:1
+
+// 10 % 5
+// // Prediction:0
+// // Actual:0
+
+// 5 % 10
+// // Prediction:5
+// // Actual:5
+
+// "Java" + "Script"
+// // Prediction:JavaScript
+// // Actual:JavaScript
+
+// " " + " "
+// // Prediction: 
+// // Actual:
+
+// " " + 0
+// // Prediction:0
+// // Actual:0
+
+// true + true
+// // Prediction:
+// // Actual:
+
+// true + false
+// // Prediction:2
+// // Actual:2
+
+// false + true
+// // Prediction:0
+// // Actual:1
+
+// false - true
+// // Prediction:1
+// // Actual:1
+
+// !true
+// // Prediction:true
+// // Actual:false
+
+// 3 - 4
+// // Prediction:-1
+// // Actual:-1
+
+// "Bob" - "bill"
+// // Prediction:NaN
+// // Actual:NaN
+
+
+
